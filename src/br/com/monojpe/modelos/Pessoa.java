e4ae5ed5cec250365d761413192fb73d0a09fade
João Pedro <joaopedro_o@outlook.com>
2025-05-14 20:01:21 -0300

    Desafio realizado

---
+++ src/br/com/monojpe/modelos/Pessoa.java
@@ -0,0 +1,16 @@
+package br.com.monojpe.modelos;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome + " Idade: " + idade;
+    }
+}
